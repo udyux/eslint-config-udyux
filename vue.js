68224c911d915ed18315b3af7687df46f7702f3a
Nicolas Udy <dev@udy.io>
2018-12-10 01:44:10 -0500

    Remove import rule for Vue

--- vue.js
+++ vue.js
@@ -5,7 +5,8 @@ module.exports = {
     'vue/max-attributes-per-line': ['warn', { singleline: 20, multiline: { max: 1, allowFirstLine: false } }],
     'vue/html-self-closing': ['error', { html: { void: 'any', normal: 'always', component: 'always' } }],
     'vue/html-closing-bracket-spacing': ['warn', { startTag: 'never', endTag: 'never', selfClosingTag: 'always' }],
-    'vue/require-default-prop': 'off'
+    'vue/require-default-prop': 'off',
+    'import/no-internal-modules': 'off'
   },
   settings: {
     'import/ignore': ['\\.s?css$'],
