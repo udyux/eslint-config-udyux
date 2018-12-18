3bbaed0e29be9709d8d1165d852d830302bcadfa
Nicolas Udy <dev@udy.io>
2018-12-18 02:25:57 -0500

    Remove conflicting rules

--- vue.js
+++ vue.js
@@ -7,8 +7,6 @@ module.exports = {
   rules: {
     'vue/singleline-html-element-content-newline': 'off',
     'vue/max-attributes-per-line': ['warn', { singleline: 20, multiline: { max: 1, allowFirstLine: false } }],
-    'vue/html-self-closing': ['error', { html: { void: 'any', normal: 'always', component: 'always' } }],
-    'vue/html-closing-bracket-spacing': ['warn', { startTag: 'never', endTag: 'never', selfClosingTag: 'always' }],
     'vue/require-default-prop': 'off'
   }
 }
