07e1e108dd2d2bb1305b1ffcda5af707cc5041e7
Nicolas Udy <dev@udy.io>
2019-01-05 02:18:27 -0500

    Add browser to env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const rules = require('./rules')
 
 module.exports = {
+  env: { browser: true },
   extends: ['eslint:recommended', 'standard', 'esnext/style-guide'],
   settings: { 'import/ignore': ['\.s?css$'] },
   rules
