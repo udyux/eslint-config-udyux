b88419d69df16d153da0fecd90e52f5deb0c40c3
Nicolas Udy <dev@udy.io>
2019-06-12 21:52:37 -0400

    Add node to env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rules = require('./rules')
 
 module.exports = {
-  env: { browser: true },
+  env: { browser: true, node: true },
   extends: ['eslint:recommended', 'standard', 'esnext/style-guide'],
   settings: { 'import/ignore': ['\\.s?css$'] },
   rules
