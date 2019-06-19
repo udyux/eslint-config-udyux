fc05c6705525fd810ef6f5b3f107072a99c9ae1a
Nicolas Udy <dev@udy.io>
2019-06-19 04:56:04 -0400

    Update vue bracket newline

--- vue.js
+++ vue.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: ['./index.js', 'plugin:vue/recommended', '@vue/prettier'],
+  extends: ['./index.js', 'plugin:vue/recommended', 'prettier'],
   settings: {
     'import/parsers': { 'vue-eslint-parser': ['.vue'] },
     'import/resolver': { alias: { map: [['@', './src']], extensions: ['.js', '.json', '.vue'] } }
   },
   rules: {
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-newline': ['warn', { singleline: 'never', multiline: 'never' }],
     'vue/max-attributes-per-line': ['warn', { singleline: 20, multiline: { max: 1, allowFirstLine: false } }],
     'vue/require-default-prop': 'off'
   }
