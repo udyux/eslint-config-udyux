cbe4824508dbc7d421bcdf674db839a7f44f263c
Nicolas Udy <dev@udy.io>
2019-08-24 22:06:15 -0400

    Update allowed unassigned imports

--- rules/import.js
+++ rules/import.js
@@ -1,4 +1,12 @@
-const unassignedImports = ['babel-polyfill', 'firebase/*', '**/startup', '**/*.loader', '**/*.css', '**/*.scss']
+const unassignedImports = [
+  'babel-polyfill',
+  'firebase/*',
+  '**/startup',
+  '**/*.polyfill',
+  '**/*.loader',
+  '**/*.css',
+  '**/*.scss'
+]
 
 module.exports = {
   'import/default': 'error',
