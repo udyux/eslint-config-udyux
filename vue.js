20e32b1211c21fadbb9f530ee85b2d562d3c0012
Nicolas Udy <dev@udy.io>
2020-02-17 15:57:09 -0500

    Upgrade dependencies and migrate settings for vue-cli@4

--- vue.js
+++ vue.js
@@ -1,13 +1,12 @@
 module.exports = {
-  extends: ['./index.js', 'plugin:vue/recommended', 'prettier'],
+  extends: ['./index.js', 'eslint:recommended', '@vue/prettier'],
   settings: {
     'import/parsers': { 'vue-eslint-parser': ['.vue'] },
     'import/resolver': { alias: { map: [['@', './src']], extensions: ['.js', '.json', '.vue'] } }
   },
   rules: {
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-closing-bracket-newline': ['warn', { singleline: 'never', multiline: 'never' }],
     'vue/max-attributes-per-line': ['warn', { singleline: 20, multiline: { max: 1, allowFirstLine: false } }],
-    'vue/require-default-prop': 'off'
+    'vue/require-default-prop': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
